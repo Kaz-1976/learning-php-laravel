e49f5a892e85f05331e54b6eba9dcdea6cde925c
Kazuhiko <kaz.saito.1976@gmail.com>
2024-08-14 01:04:39 +0900

    20240813

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'user_id' => ['required', 'string', 'max:255'],
+            'user_id' => ['required', 'string', 'max:255', 'unique:users'],
             'user_name' => ['required', 'string', 'max:255'],
             'user_kana' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
