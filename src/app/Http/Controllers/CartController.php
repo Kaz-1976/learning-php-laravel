509c1f74172f4c9d6f39944fe14040cd16019e48
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-08-30 15:03:32 +0900

    20240830

--- src/app/Http/Controllers/CartController.php
+++ src/app/Http/Controllers/CartController.php
@@ -4,17 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\EcCartDetail;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
     public function index()
     {
         // カート情報
-        $ec_cart_details = EcCartDetail::query()
-            ->find(session()->get('cart-id'))
-            ->with(['ec_products:id,name,image_data,image_type,qty,price'])
-            ->orderBy('id', 'asc')
-            ->paginate(6);
+        if (empty(Auth::user()->cart_id)) {
+            $ec_cart_details = null;
+        } else {
+            $ec_cart_details = EcCartDetail::query()
+                ->with(['ec_products:id,name,image_data,image_type,qty,price'])
+                ->find(Auth::user()->cart_id)
+                ->orderBy('id', 'asc')
+                ->paginate(6);
+        }
         //
         return view('ec_site.cart', ['ec_cart_details' => $ec_cart_details]);
     }
