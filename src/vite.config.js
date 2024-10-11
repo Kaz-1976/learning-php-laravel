b29c5fdf617a0be16cdfec305ec8d4a6037d4640
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-10-11 11:33:20 +0900

    20241011

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     server: {
         hmr: {
-            host: 'localhost',
+            host: "localhost",
         },
     },
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                "resources/css/app.css",
+                "resources/sass/app.scss",
+                "resources/js/app.js",
             ],
             refresh: true,
         }),
