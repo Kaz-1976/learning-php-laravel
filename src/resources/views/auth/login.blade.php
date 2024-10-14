24606b5cc2ee1345031e2fc39bbaf8ccf1486a80
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-10-14 11:47:24 +0900

    20241014

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -53,7 +53,7 @@
     </form>
     @if (Route::has('register'))
     <div class="container my-4 text-center text-gray-600 dark:text-gray-400">
-        アカウントをお持ちでない方は
+        <span>アカウントをお持ちでない方は</span>
         <a class="underline font-bold text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
             href="{{ url('ec_site/register', null, app()->isProduction()) }}">
             {{ __('Register') }}
