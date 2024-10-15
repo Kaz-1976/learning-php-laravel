0217919afbd4f8160d9fd19882e2f90b600affc6
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-10-15 10:40:08 +0900

    20241015

--- src/database/migrations/2024_10_15_103823_add_password_reset_tokens_table.php
+++ src/database/migrations/2024_10_15_103823_add_password_reset_tokens_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('admin_password_reset_tokens', function (Blueprint $table) {
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamps();
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('admin_password_reset_tokens');
+        Schema::dropIfExists('password_reset_tokens');
     }
 };
