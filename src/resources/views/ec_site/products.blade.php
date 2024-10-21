141477a68082574c8364a48bf0ceb4f414c5ae8f
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-10-21 15:38:56 +0900

    20241020

--- src/resources/views/ec_site/products.blade.php
+++ src/resources/views/ec_site/products.blade.php
@@ -5,89 +5,87 @@
 
 {{-- ページコンテンツ --}}
 @section('content')
-<div class="container flex flex-col gap-4">
-    <div class="container p-4 border-solid border-2 rounded-lg border-sky-950 dark:border-sky-50">
-        <form class="flex flex-row gap-4" id="register"
-            action="{{ url('ec_site/admin/products/store', null, app()->isProduction()) }}" method="POST"
-            enctype="multipart/form-data">
-            @csrf
-            <div class="flex flex-col grow-0 basis-80 h-80 m-auto">
-                <div class="flex w-80 h-80 border-solid border-2 border-sky-50">
-                    <img id="register-image-preview">
-                </div>
+<div class="flex flex-col gap-4">
+    <form class="flex flex-col gap-4 p-4 border-solid border-2 rounded-lg border-sky-950 dark:border-sky-50"
+        id="register" action="{{ url('ec_site/admin/products/store', null, app()->isProduction()) }}" method="POST"
+        enctype="multipart/form-data">
+        @csrf
+        <div class="flex flex-col grow-0 basis-80 h-80 m-auto">
+            <div class="flex w-80 h-80 border-solid border-2 border-sky-50">
+                <img id="register-image-preview">
             </div>
-            <div class="flex flex-col grow">
-                {{-- 商品名 --}}
-                <div>
-                    <x-input-label for="register-product-name" :value="__('商品名')" />
-                    <x-text-input class="block mt-1 w-full" type="text" id="register-product-name" name="name"
-                        :value="old('name')" required autofocus />
+        </div>
+        <div class="flex flex-col grow">
+            {{-- 商品名 --}}
+            <div>
+                <x-input-label for="register-product-name" :value="__('商品名')" />
+                <x-text-input class="block mt-1 w-full" type="text" id="register-product-name" name="name"
+                    :value="old('name')" required autofocus />
+                @if (old('register') == '1')
+                <x-input-error :messages="$errors->get('name')" class="my-2" />
+                @endif
+            </div>
+            {{-- 数量 --}}
+            <div>
+                <x-input-label for="register-qty" :value="__('数量')" />
+                <div class="flex flex-row content-stretch gap-2">
+                    <x-text-input class="basis-auto mt-1 w-full text-right" type="number" id="register-qty" name="qty"
+                        :value="old('qty')" required autofocus />
+                    <x-input-label class="mt-auto" for="register-qty" :value="__('点')" />
+                </div>
+                @if (old('register') == '1')
+                <x-input-error :messages="$errors->get('qty')" class="my-2" />
+                @endif
+            </div>
+            {{-- 価格 --}}
+            <div>
+                <x-input-label for="register-price" :value="__('価格')" />
+                <div class="flex flex-row content-stretch gap-2">
+                    <x-text-input class="block mt-1 w-full text-right" type="number" id="register-price" name="price"
+                        :value="old('price')" required autofocus />
+                    <x-input-label class="mt-auto" for="register-qty" :value="__('円')" />
+                </div>
+                @if (old('register') == '1')
+                <x-input-error :messages="$errors->get('price')" class="my-2" />
+                @endif
+            </div>
+            {{-- 画像 --}}
+            <div>
+                <x-input-label for="register-image" :value="__('画像')" />
+                <x-text-input class="block mt-1 w-full" type="file" id="register-image" name="image" required
+                    accept="image/*" />
+                @if (old('register') == '1')
+                <x-input-error :messages="$errors->get('image')" class="my-2" />
+                @endif
+            </div>
+            <div class="pt-4 flex flex-row justify-between gap-2">
+                <div class="my-auto flex flex-row basis-1/5">
+                    <x-text-input class="w-8 h-8" type="checkbox" id="register-public" name="public_flg"
+                        :value="old('public_flg')" autofocus />
+                    <x-input-label class="my-auto pl-2 flex" for="register-public" :value="__('公開')" />
                     @if (old('register') == '1')
-                    <x-input-error :messages="$errors->get('name')" class="my-2" />
+                    <x-input-error :messages="$errors->get('public_flg')" class="my-2" />
                     @endif
                 </div>
-                {{-- 数量 --}}
-                <div>
-                    <x-input-label for="register-qty" :value="__('数量')" />
-                    <div class="flex flex-row content-stretch gap-2">
-                        <x-text-input class="basis-auto mt-1 w-full text-right" type="number" id="register-qty"
-                            name="qty" :value="old('qty')" required autofocus />
-                        <x-input-label class="mt-auto" for="register-qty" :value="__('点')" />
+                <div class="flex flex-row-reverse basis-4/5 gap-2">
+                    <div class="my-auto flex flex-row basis-1/2">
+                        <x-primary-button class="flex basis-full" name="register" :value="1">
+                            <span class="flex m-auto text-2xl text-center font-bold">{{ __('Register') }}</span>
+                        </x-primary-button>
                     </div>
-                    @if (old('register') == '1')
-                    <x-input-error :messages="$errors->get('qty')" class="my-2" />
-                    @endif
-                </div>
-                {{-- 価格 --}}
-                <div>
-                    <x-input-label for="register-price" :value="__('価格')" />
-                    <div class="flex flex-row content-stretch gap-2">
-                        <x-text-input class="block mt-1 w-full text-right" type="number" id="register-price"
-                            name="price" :value="old('price')" required autofocus />
-                        <x-input-label class="mt-auto" for="register-qty" :value="__('円')" />
-                    </div>
-                    @if (old('register') == '1')
-                    <x-input-error :messages="$errors->get('price')" class="my-2" />
-                    @endif
-                </div>
-                {{-- 画像 --}}
-                <div>
-                    <x-input-label for="register-image" :value="__('画像')" />
-                    <x-text-input class="block mt-1 w-full" type="file" id="register-image" name="image" required
-                        accept="image/*" />
-                    @if (old('register') == '1')
-                    <x-input-error :messages="$errors->get('image')" class="my-2" />
-                    @endif
-                </div>
-                <div class="pt-4 flex flex-row justify-between gap-2">
-                    <div class="my-auto flex flex-row basis-1/5">
-                        <x-text-input class="w-8 h-8" type="checkbox" id="register-public" name="public_flg"
-                            :value="old('public_flg')" autofocus />
-                        <x-input-label class="my-auto pl-2 flex" for="register-public" :value="__('公開')" />
-                        @if (old('register') == '1')
-                        <x-input-error :messages="$errors->get('public_flg')" class="my-2" />
-                        @endif
-                    </div>
-                    <div class="flex flex-row-reverse basis-4/5 gap-2">
-                        <div class="my-auto flex flex-row basis-1/2">
-                            <x-primary-button class="flex basis-full" name="register" :value="1">
-                                <span class="flex m-auto text-2xl text-center font-bold">{{ __('Register') }}</span>
-                            </x-primary-button>
-                        </div>
-                        <div class="my-auto flex flex-row basis-1/2">
-                            <x-secondary-button class="flex basis-full" type='reset' name="reset" :value="1">
-                                <span class="flex m-auto text-2xl text-center font-bold">{{ __('リセット') }}</span>
-                            </x-secondary-button>
-                        </div>
+                    <div class="my-auto flex flex-row basis-1/2">
+                        <x-secondary-button class="flex basis-full" type='reset' name="reset" :value="1">
+                            <span class="flex m-auto text-2xl text-center font-bold">{{ __('リセット') }}</span>
+                        </x-secondary-button>
                     </div>
                 </div>
             </div>
-        </form>
-    </div>
-    <div class="container w-full">
+        </div>
+    </form>
+    <div class="w-full">
         {{ $ec_products->render() }}
     </div>
-    <div class="container">
+    <div class="block">
         @php
         $id = [];
         $form = [];
@@ -184,7 +182,7 @@
         </div>
         @endforeach
     </div>
-    <div class="container w-full">
+    <div class="w-full">
         {{ $ec_products->render() }}
     </div>
 </div>
