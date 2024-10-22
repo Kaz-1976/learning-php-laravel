4f0edebe9a6aa9ed2401288d9083378d6d69e1a3
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-10-22 15:34:10 +0900

    20241021

--- src/resources/views/ec_site/products.blade.php
+++ src/resources/views/ec_site/products.blade.php
@@ -112,8 +112,8 @@
                             id="{{ $form[$ec_product->id] }}-image-preview" src="{{ $image[$ec_product->id] }}">
                     </div>
                 </div>
-                <div class="flex flex-col md:flex-row grow gap-2">
-                    <div class="flex flex-col basis-4/5 gap-1">
+                <div class="flex flex-col md:flex-row grow gap-4">
+                    <div class="flex flex-col basis-4/5 gap-2">
                         <div class="block">
                             <x-input-label for="{{ $form[$ec_product->id] }}-name" :value="__('名称')" />
                             <x-text-input class="block mt-1 w-full" type="text" id="{{ $form[$ec_product->id] }}-name"
