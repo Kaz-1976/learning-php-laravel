6813ef1e5e5247ae80687bfda1a07e7588ff3eee
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-11-04 20:12:48 +0900

    20241104

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = url('/ec_site/', null, app()->isProduction());
+    protected $redirectTo = 'ec_site.index';
 
     /**
      * Create a new controller instance.
