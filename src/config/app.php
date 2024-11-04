0b20e654040af7b675812664cede46964b432bcc
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-11-05 00:08:59 +0900

    20241104

--- src/config/app.php
+++ src/config/app.php
@@ -122,7 +122,4 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
-    //  Add
-    'asset_url' => env('ASSET_URL', null),
 ];
