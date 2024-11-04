e0e9238eb54e2552b147499b2abf77b36961c9cc
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-11-04 20:06:28 +0900

    20241104

--- src/app/Http/Controllers/IndexController.php
+++ src/app/Http/Controllers/IndexController.php
@@ -15,13 +15,13 @@ class IndexController extends Controller
             // 管理／一般
             if ($user->admin_flg == 0) {
                 // 一般ユーザーは商品一覧へ
-                return redirect(url('ec_site/items', null, app()->isProduction()));
+                return redirect(url('/ec_site/items', null, app()->isProduction()));
             } else {
                 // 管理ユーザーは管理メニューへ
-                return redirect(url('ec_site/admin', null, app()->isProduction()));
+                return redirect(url('/ec_site/admin', null, app()->isProduction()));
             }
         }
         // ログインしていない場合はログインページへ
-        return redirect(url('ec_site/login', null, app()->isProduction()));
+        return redirect(url('/ec_site/login', null, app()->isProduction()));
     }
 }
