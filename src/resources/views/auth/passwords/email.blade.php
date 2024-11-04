943d116798239118947e4f31b4d9a7f98a6a10ee
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-11-04 23:17:54 +0900

    20241104

--- src/resources/views/auth/passwords/email.blade.php
+++ src/resources/views/auth/passwords/email.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    <form method="POST" action="{{ route('password.email') }}">
+                    <form method="POST" action="{{ url('ec_site/forgot-password', null, app()->isProduction()) }}">
                         @csrf
 
                         <div class="row mb-3">
