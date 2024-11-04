6621988ceb02d2277aaa9c27797cbb2a24085333
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-11-05 01:35:39 +0900

    20241104

--- src/app/Notifications/ResetPassword.php
+++ src/app/Notifications/ResetPassword.php
@@ -63,10 +63,10 @@ class ResetPassword extends ResetPasswordNotification
     protected function buildMailMessage($url)
     {
         return (new MailMessage)
-            ->subject('Reset Password Notification')
-            ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', url(config('app.url') . route('password.reset', $this->token, false)))
-            ->line('If you did not request a password reset, no further action is required.');
+            ->subject(__('Reset Password Notification'))
+            ->line(__('You are receiving this email because we received a password reset request for your account.'))
+            ->action(__('Reset Password'), url(config('app.url') . route('password.reset', $this->token, false)))
+            ->line(__('If you did not request a password reset, no further action is required.'));
     }
 
     /**
