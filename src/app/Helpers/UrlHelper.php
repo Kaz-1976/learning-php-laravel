77ca5d9b94e23352f71033282b28600fb0da1836
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-11-07 00:07:06 +0900

    20241106

--- src/app/Helpers/UrlHelper.php
+++ src/app/Helpers/UrlHelper.php
@@ -7,17 +7,17 @@ class UrlHelper
     public static function generateUrl($path)
     {
         $baseUrl = rtrim(config('app.url'), '/');
-        $url_path = parse_url($path, PHP_URL_PATH);
-        $url_query = parse_url($path, PHP_URL_QUERY);
+        $urlPath = parse_url($path, PHP_URL_PATH);
+        $urlQuery = parse_url($path, PHP_URL_QUERY);
 
         // フルURLが含まれているかチェック
         if (filter_var($path, FILTER_VALIDATE_URL)) {
             // サブディレクトリが含まれていない場合のみ追加
             if (strpos($path, $baseUrl) === false) {
-                return $baseUrl . '/' . ltrim($url_path, '/') . '?' . ltrim($url_query, '?');
+                return $baseUrl . '/' . ltrim($urlPath, '/') . '?' . ltrim($urlQuery, '?');
             }
             return $path;
         }
-        return $baseUrl . '/' . ltrim($url_path, '/') . '?' . ltrim($url_query, '?');
+        return $baseUrl . '/' . ltrim($urlPath, '/') . '?' . ltrim($urlQuery, '?');
     }
 }
