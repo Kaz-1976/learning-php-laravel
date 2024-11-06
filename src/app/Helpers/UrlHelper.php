356186bf857a48b3b07dfb3fadfa7ec19de079f4
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-11-06 23:24:44 +0900

    20241106

--- src/app/Helpers/UrlHelper.php
+++ src/app/Helpers/UrlHelper.php
@@ -10,12 +10,8 @@ class UrlHelper
 
         // フルURLが含まれているかチェック
         if (filter_var($path, FILTER_VALIDATE_URL)) {
-            // サブディレクトリが含まれていない場合のみ追加
-            if (strpos($path, $baseUrl) === false) {
-                $url_path = parse_url($path, PHP_URL_PATH);
-                return $baseUrl . '/' . ltrim($url_path, '/');
-            }
-            return $path;
+            $url_path = parse_url($path, PHP_URL_PATH);
+            return $baseUrl . '/' . ltrim($url_path, '/');
         }
 
         return $baseUrl . '/' . ltrim($path, '/');
