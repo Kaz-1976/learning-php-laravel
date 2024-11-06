eae4a9c1e398b83b0d26d48704e533b81f309abc
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-11-06 23:09:46 +0900

    20241106

--- src/app/Helpers/UrlHelper.php
+++ src/app/Helpers/UrlHelper.php
@@ -7,6 +7,12 @@ class UrlHelper
     public static function generateUrl($path)
     {
         $baseUrl = config('app.url'); // APP_URL環境変数を取得
+
+        // フルURLがすでに含まれている場合はそのまま返す
+        if (filter_var($path, FILTER_VALIDATE_URL)) {
+            return $path;
+        }
+
         return rtrim($baseUrl, '/') . '/' . ltrim($path, '/');
     }
 }
