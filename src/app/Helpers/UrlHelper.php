dc47acf92f4244748ea02edd4c6f892f29aab42a
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2024-11-06 23:58:39 +0900

    20241106

--- src/app/Helpers/UrlHelper.php
+++ src/app/Helpers/UrlHelper.php
@@ -7,13 +7,13 @@ class UrlHelper
     public static function generateUrl($path)
     {
         $baseUrl = rtrim(config('app.url'), '/');
+        $url_path = parse_url($path, PHP_URL_PATH);
+        $url_query = parse_url($path, PHP_URL_QUERY);
 
         // フルURLが含まれているかチェック
         if (filter_var($path, FILTER_VALIDATE_URL)) {
             // サブディレクトリが含まれていない場合のみ追加
             if (strpos($path, $baseUrl) === false) {
-                $url_path = parse_url($path, PHP_URL_PATH);
-                $url_query = parse_url($path, PHP_URL_QUERY);
                 return $baseUrl . '/' . ltrim($url_path, '/') . '?' . ltrim($url_query, '?');
             }
             return $path;
