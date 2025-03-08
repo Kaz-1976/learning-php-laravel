4bdf018bc1e7f0c559a72d971f4a8ca4660fd313
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2025-03-09 06:19:41 +0900

    20250309

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Api\AddressController;
 use App\Http\Controllers\Api\ProductImageController;
 use App\Http\Controllers\Api\ReceiptImageController;
 
-Route::prefix('ec_site')->group(function () {
-    Route::get('/zip/{code}', [ZipController::class, 'zip'])->where('code', '[0-9]{7}');
-    Route::get('/address/{id}', [AddressController::class, 'address'])->where('user', '[0-9]+')->where('id', '[0-9]+');
-    Route::get('/product-image/{id}', [ProductImageController::class, 'image'])->where('id', '[0-9]+');
-    Route::get('/receipt-image/{id}/{no}', [ReceiptImageController::class, 'image'])->where('id', '[0-9]+')->where('no', '[0-9]+');
-});
+// Route::prefix('ec_site')->group(function () {
+Route::get('/zip/{code}', [ZipController::class, 'zip'])->where('code', '[0-9]{7}');
+Route::get('/address/{id}', [AddressController::class, 'address'])->where('user', '[0-9]+')->where('id', '[0-9]+');
+Route::get('/product-image/{id}', [ProductImageController::class, 'image'])->where('id', '[0-9]+');
+Route::get('/receipt-image/{id}/{no}', [ReceiptImageController::class, 'image'])->where('id', '[0-9]+')->where('no', '[0-9]+');
+// });
