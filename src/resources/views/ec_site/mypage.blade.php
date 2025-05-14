e13bd5fa3409ee65a3cf6c45da34a9413b9b5e23
Kazuhiko Saito <kaz.saito.1976@gmail.com>
2025-05-14 14:29:36 +0900

    20250514

--- src/resources/views/ec_site/mypage.blade.php
+++ src/resources/views/ec_site/mypage.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <x-menu-button-box>
         <x-menu-button href="{{ url('mypage/profile', null, app()->isProduction()) }}">個人情報</x-menu-button>
-        <x-menu-button href="{{ url('mypage/address', null, app()->isProduction()) }}">宛先情報</x-menu-button>
+        <x-menu-button href="{{ url('mypage/address', null, app()->isProduction()) }}">配送先情報</x-menu-button>
         <x-menu-button href="{{ url('mypage/receipt', null, app()->isProduction()) }}">購入履歴</x-menu-button>
     </x-menu-button-box>
 @endsection
--- src/resources/views/ec_site/shipping.blade.php
+++ src/resources/views/ec_site/shipping.blade.php
@@ -6,12 +6,12 @@
 {{-- ページコンテンツ --}}
 @section('content')
     <div class="flex flex-col gap-4">
-        <form class="flex flex-col gap-4 p-4 border-solid border-2 rounded-lg border-sky-950 dark:border-sky-50"
+        <form class="flex flex-col gap-4 rounded-lg border-2 border-solid border-sky-950 p-4 dark:border-sky-50"
             id="register" action="{{ url('shipping/store', null, app()->isProduction()) }}" method="POST">
             @csrf
             {{-- 宛先名 --}}
-            <div class="flex flex-row basis-full gap-2">
-                <div class="flex flex-col basis-1/2 justify-stretch">
+            <div class="flex basis-full flex-row gap-2">
+                <div class="flex basis-1/2 flex-col justify-stretch">
                     <select class="" id="id" name="id" required>
                         <option value="" selected>宛先を選択してください</option>
                         @foreach ($ecAddresses as $ecAddress)
@@ -21,39 +21,39 @@
                         @endforeach
                     </select>
                 </div>
-                <div class="flex flex-row basis-1/2 my-auto">
-                    <x-primary-button class="flex basis-full" form="register" name="register-select" :value="0">
-                        <span class="flex m-auto text-base md:text-xl text-center font-bold">{{ __('決定') }}</span>
+                <div class="my-auto flex basis-1/2 flex-row">
+                    <x-primary-button class="flex basis-full" name="register-select" form="register" :value="0">
+                        <span class="m-auto flex text-center text-base font-bold md:text-xl">{{ __('決定') }}</span>
                     </x-primary-button>
                 </div>
             </div>
-            <div class="flex flex-row basis-full gap-2">
+            <div class="flex basis-full flex-row gap-2">
                 {{-- 郵便番号 --}}
-                <div class="flex flex-col basis-1/3 justify-stretch">
+                <div class="flex basis-1/3 flex-col justify-stretch">
                     <x-input-label for="zip" :value="__('郵便番号')" />
-                    <x-text-input class="block mt-1 w-full" type="text" id="zip" name="zip" :value="''"
+                    <x-text-input class="mt-1 block w-full" id="zip" name="zip" type="text" :value="''"
                         :disabled="true" />
                 </div>
                 {{-- 都道府県 --}}
-                <div class="flex flex-col basis-2/3 justify-stretch">
+                <div class="flex basis-2/3 flex-col justify-stretch">
                     <x-input-label for="pref" :value="__('都道府県')" />
-                    <x-text-input class="block mt-1 w-full" type="text" id="pref" name="pref" :value="''"
+                    <x-text-input class="mt-1 block w-full" id="pref" name="pref" type="text" :value="''"
                         :disabled="true" />
                 </div>
             </div>
             {{-- 住所１ --}}
-            <div class="flex flex-row basis-full gap-2">
-                <div class="flex flex-col basis-full justify-stretch">
+            <div class="flex basis-full flex-row gap-2">
+                <div class="flex basis-full flex-col justify-stretch">
                     <x-input-label for="address1" :value="__('住所１')" />
-                    <x-text-input class="flex mt-1 w-full" type="text" id="address1" name="address1" :value="''"
+                    <x-text-input class="mt-1 flex w-full" id="address1" name="address1" type="text" :value="''"
                         :disabled="true" />
                 </div>
             </div>
             {{-- 住所２ --}}
-            <div class="flex flex-row basis-full gap-2">
-                <div class="flex flex-col basis-full justify-stretch">
+            <div class="flex basis-full flex-row gap-2">
+                <div class="flex basis-full flex-col justify-stretch">
                     <x-input-label for="address2" :value="__('住所２')" />
-                    <x-text-input class="flex mt-1 w-full" type="text" id="address2" name="address2" :value="''"
+                    <x-text-input class="mt-1 flex w-full" id="address2" name="address2" type="text" :value="''"
                         :disabled="true" />
                 </div>
             </div>
